897376d3a697097a1bf86641f155ce3644be2679
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-10-19 09:47:33 +0200

    clickShiftingContentLink() added

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,6 +1,5 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -8,11 +7,6 @@ public class HomePage extends BasePage {
 
     public HomePage(WebDriver driver) {
         super(driver);
-    }
-
-    private void clickLink(String linkText) {
-        getWait().until(ExpectedConditions.visibilityOfElementLocated(By.linkText(linkText)))
-                .click();
     }
 
     public DropdownPage clickDropdownLink() {
@@ -25,5 +19,11 @@ public class HomePage extends BasePage {
         clickLink("Form Authentication");
         getWait().until(ExpectedConditions.urlContains("login"));
         return new LoginPage(getDriver());
+    }
+
+    public ShiftingContentPage clickShiftingContentLink() {
+        clickLink("Shifting Content");
+        getWait().until(ExpectedConditions.urlContains("shifting_content"));
+        return new ShiftingContentPage(getDriver());
     }
 }
