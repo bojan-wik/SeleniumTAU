63d74d50909dda2c51dc9a641551a82a4552cf4f
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-10-20 15:28:39 +0200

    adding Actions field and method

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,10 +12,12 @@ public class BasePage {
 
     private WebDriver driver;
     private WebDriverWait wait;
+    private Actions actions;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, TIMEOUT);
+        this.actions = new Actions(driver);
     }
 
     public WebDriver getDriver() {
@@ -23,6 +26,10 @@ public class BasePage {
 
     public WebDriverWait getWait() {
         return wait;
+    }
+
+    public Actions getActions() {
+        return actions;
     }
 
     public void clickLink(String linkText) {
