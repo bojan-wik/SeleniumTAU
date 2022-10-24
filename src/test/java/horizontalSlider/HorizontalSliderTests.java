3d37c59ea32987d505c5b3efc73cb5a2b140e932
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-10-24 12:04:00 +0200

    HorizontalSliderPage + HorizontalSliderTests classes added

--- src/test/java/horizontalSlider/HorizontalSliderTests.java
+++ src/test/java/horizontalSlider/HorizontalSliderTests.java
@@ -1,2 +1,16 @@
-package horizontalSlider;public class HorizontalSliderTests {
+package horizontalSlider;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.HorizontalSliderPage;
+
+public class HorizontalSliderTests extends BaseTest {
+
+    @Test
+    public void test1() {
+        HorizontalSliderPage horizontalSliderPage = homePage.clickHorizontalSliderLink();
+//        horizontalSliderPage.clickOnSlider();
+        horizontalSliderPage.moveSlider("right");
+        System.out.println(horizontalSliderPage.getResult());
+    }
 }
