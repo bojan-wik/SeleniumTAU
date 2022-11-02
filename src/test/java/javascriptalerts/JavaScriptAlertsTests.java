bc6d4fd4ce596a24391c0020be1e54b1bc4a0893
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-11-02 12:52:42 +0100

    getTextFromConfirmPopupTest() test method added

--- src/test/java/javascriptalerts/JavaScriptAlertsTests.java
+++ src/test/java/javascriptalerts/JavaScriptAlertsTests.java
@@ -8,10 +8,19 @@ import pages.JavaScriptAlertsPage;
 public class JavaScriptAlertsTests extends BaseTest {
 
     @Test
-    public void acceptAlertTest() {
+    public void acceptAlertPopupTest() {
         JavaScriptAlertsPage javaScriptAlertsPage = homePage.clickJavaScriptAlertsLink();
-        javaScriptAlertsPage.triggerAlert().clickOkButtonOnJsAlert();
+        javaScriptAlertsPage.triggerAlertPopup().popup_clickToAccept();
         Assert.assertEquals(javaScriptAlertsPage.getResultText(), "You successfully clicked an alert",
                 "Result text incorrect");
+    }
+
+    @Test
+    public void getTextFromConfirmPopupTest() {
+        JavaScriptAlertsPage javaScriptAlertsPage = homePage.clickJavaScriptAlertsLink();
+        javaScriptAlertsPage.triggerConfirmPopup();
+        Assert.assertEquals(javaScriptAlertsPage.popup_getText(), "I am a JS Confirm",
+                "Confirm popup text incorrect");
+        javaScriptAlertsPage.popup_clickToDismiss();
     }
 }
