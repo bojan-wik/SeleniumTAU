a4fcb4b1f87c77d5a57b72a5f5ae8d97ba208289
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-11-03 09:26:19 +0100

    enterTextInPromptPopupTest() test method added

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
 public class BaseTest {
@@ -15,12 +16,17 @@ public class BaseTest {
 
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Tools\\Webdrivers\\Chrome\\106\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Tools\\Webdrivers\\Chrome\\107\\chromedriver.exe");
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 5);
-        driver.get("https://the-internet.herokuapp.com/");
+        goHome();
         driver.manage().window().maximize();
         homePage = new HomePage(driver);
+    }
+
+    @BeforeMethod
+    public void goHome() {
+        driver.get("https://the-internet.herokuapp.com/");
     }
 
 //    @AfterClass
