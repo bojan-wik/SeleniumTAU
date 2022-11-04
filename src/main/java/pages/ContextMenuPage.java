3f7abb62959342c51e4f0a7bc53e82d911396175
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-11-04 14:32:20 +0100

    ContextMenuPage + ContextMenuTests classes added

---
+++ src/main/java/pages/ContextMenuPage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class ContextMenuPage extends BasePage {
+
+    public ContextMenuPage(WebDriver driver) {
+        super(driver);
+    }
+
+    private By box = By.id("hot-spot");
+
+    public void rightClickInBox() {
+        getWait().until(ExpectedConditions.elementToBeClickable(box));
+        getActions().contextClick(getDriver().findElement(box)).perform();
+    }
+
+    // TODO: 04.11.2022 po co powielać tą metodę
+//    public String popup_getText
+}
