9067eac9f123daebf69f15ed2d47353214a59b2c
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-11-14 12:22:05 +0100

    FramesTests.java: nestedFramesTest() added

--- src/test/java/frames/FramesTests.java
+++ src/test/java/frames/FramesTests.java
@@ -3,6 +3,8 @@ package frames;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.FramesPage;
+import pages.NestedFramesPage;
 import pages.WysiwygEditorPage;
 
 public class FramesTests extends BaseTest {
@@ -22,5 +24,16 @@ public class FramesTests extends BaseTest {
 
         Assert.assertEquals(wysiwygEditorPage.getTextFromEditor(), text1 + text2,
                 "Text from the Editor incorrect");
+    }
+
+    @Test
+    public void nestedFramesTest() {
+        FramesPage framesPage = homePage.clickFramesLink();
+        NestedFramesPage nestedFramesPage = framesPage.clickNestedFramesLink();
+
+        Assert.assertEquals(nestedFramesPage.getTextFromFrame("left"), "LEFT",
+                "Text from left frame incorrect");
+        Assert.assertEquals(nestedFramesPage.getTextFromFrame("bottom"), "BOTTOM",
+                "Text from bottom frame incorrect");
     }
 }
