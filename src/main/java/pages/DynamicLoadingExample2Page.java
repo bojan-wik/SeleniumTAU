f38b562a1222c5be2ae4b512506abc475a8790e7
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-11-17 12:15:26 +0100

    WaitTests.waitUntilRenderedTest() added

---
+++ src/main/java/pages/DynamicLoadingExample2Page.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class DynamicLoadingExample2Page extends BasePage {
+
+    public DynamicLoadingExample2Page(WebDriver driver) {
+        super(driver);
+    }
+
+    private By startButton = By.cssSelector("#start > button");
+    private By helloWorldBox = By.cssSelector("#finish h4");
+
+    public void clickStartButton() {
+        getWait().until(ExpectedConditions.elementToBeClickable(startButton)).click();
+    }
+
+    public String getHelloWorldText() {
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(helloWorldBox)).getText();
+    }
+}
