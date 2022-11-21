65c91538fe11b3dcb1188fe7789dd16269987384
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-11-21 13:54:24 +0100

    added: JavaScriptTests.addMultipleAttributeToDropdownTest()

--- src/test/java/javascript/JavaScriptTests.java
+++ src/test/java/javascript/JavaScriptTests.java
@@ -1,9 +1,13 @@
 package javascript;
 
 import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.InfiniteScrollPage;
+import pages.DropdownPage;
 import pages.LargeAndDeepDom;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JavaScriptTests extends BaseTest {
 
@@ -16,5 +20,20 @@ public class JavaScriptTests extends BaseTest {
     @Test
     public void infiniteScrollTest() {
         homePage.clickInfiniteScrollLink().scrollToNthParagraph(5);
+    }
+
+    @Test
+    public void addMultipleAttributeToDropdownTest() {
+        DropdownPage dropdownPage = homePage.clickDropdownLink();
+        dropdownPage.addMultipleAttribute();
+
+        List<String> optionsToSelect = new ArrayList<>(List.of("Option 1", "Option 2"));
+        for (String option : optionsToSelect) {
+            dropdownPage.selectOption(option);
+        }
+
+        List<String> selectedOptions = dropdownPage.getSelectedOptions();
+        Assert.assertEquals(selectedOptions.size(), 2, "Incorrect number of selections");
+        Assert.assertTrue(selectedOptions.containsAll(optionsToSelect), "Incorrect options selected");
     }
 }
