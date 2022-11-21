91b74a47823f5007a4650a8996ba19d7d7bfa0de
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-11-21 12:30:50 +0100

    added: InfiniteScrollPage.java, JavaScriptTests.java

---
+++ src/main/java/pages/InfiniteScrollPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class InfiniteScrollPage extends BasePage {
+
+    private By paragraph = By.className("jscroll-added");
+
+    public InfiniteScrollPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void scrollToNthParagraph(int n) {
+        String script =  "window.scrollTo(0, document.body.scrollHeight)";
+        JavascriptExecutor jsExecutor =  (JavascriptExecutor)getDriver();
+
+        while (getNumberOfParagraphsPresent() < n) {
+            jsExecutor.executeScript(script);
+        }
+    }
+
+    private int getNumberOfParagraphsPresent() {
+        return getDriver().findElements(paragraph).size();
+    }
+}
