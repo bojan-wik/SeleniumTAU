5eab238ebfa7eabebd5da24e173ad07ccceb8a55
bojanoww <wiktor.bojanowski@contractors.roche.com>
2022-12-05 10:32:16 +0100

    BaseTest.takeScreenshotIfTestFails() method added

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,13 +1,20 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.BrowserGetter;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -31,6 +38,19 @@ public class BaseTest {
     @BeforeMethod
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    @AfterMethod
+    public void takeScreenshotIfTestFails(ITestResult result) {
+        if (!result.isSuccess()) {
+            TakesScreenshot camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("screenshots/" + result.getName() + ".png"));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
